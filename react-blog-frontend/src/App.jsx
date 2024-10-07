866af86351f147a88f9a6e17f2e23ec419c318b6
Md Saiful Islam <46101909+saifulislam07@users.noreply.github.com>
2024-10-07 06:45:23 +0600

    first commit

---
+++ react-blog-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function App() {
+
+  return (
+    <>
+      <h1>React Laravel blog</h1>
+    </>
+  )
+}
+
+export default App
