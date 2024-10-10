81f7d76085fbff80c3f40fa08c3c16535db8ef8c
Md Saiful Islam <46101909+saifulislam07@users.noreply.github.com>
2024-10-10 11:09:32 +0600

    blog search

--- react-blog-frontend/src/components/Blogs.jsx
+++ react-blog-frontend/src/components/Blogs.jsx
@@ -4,12 +4,24 @@ import BlogCard from './BlogCard'
 const Blogs = () => {
 
     const [blogs, setBlogs] = useState();
+    const [keyword, setKeyword] = useState('');
 
     const fatchBlogs = async () => {
         const res = await fetch('http://127.0.0.1:8000/api/blogs');
         const result = await res.json();
         setBlogs(result.data);
-        console.log(result);
+    }
+
+    const resetSearch = () => {
+        fatchBlogs();
+        setKeyword('');
+    }
+
+    const searchBlogs = async (e) => {
+        e.preventDefault();
+        const res = await fetch('http://127.0.0.1:8000/api/blogs?keyword=' + keyword);
+        const result = await res.json();
+        setBlogs(result.data);
     }
 
     useEffect(() => {
@@ -18,6 +30,17 @@ const Blogs = () => {
 
     return (
         <div className='container'>
+            <div className="d-flex justify-content-center pt-5 mb-4">
+                <form onSubmit={(e) => searchBlogs(e)}>
+
+                    <div className='d-flex'>
+                        <input type="text" value={keyword} onChange={(e) => setKeyword(e.target.value)} className='form-control' placeholder='Search Blogs' />
+                        <button className='btn btn-dark ms-2'>Search</button>
+                        <button type='button' onClick={() => resetSearch()} className='btn btn-success ms-2'>Reset</button>
+                    </div>
+                </form>
+            </div>
+
             <div className='d-flex justify-content-between pt-5 mb-4'>
                 <h4>Blogs</h4>
                 <a href="/create" className='btn btn-dark'>Create</a>
